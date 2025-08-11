72d6899c43755123aa80246cd8b0ae167ef10e62
Flaco <lifugodwin7@gmail.comgit config --global user.email lifugodwin7@gmail.com>
2025-08-11 10:51:18 +0100

    added custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
   /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
+   content: ["./public/**/*.{html,js}"],
    theme: {
      extend: {
       colors:{
         text:"rgb(52,70,77)",
         background:"rgb(205,162,117)",
-        our:"rgb(4,48,64)"
+        our:"rgb(4,48,64)",
+        fade:"rgb(243,234,223)",
+        whitte:"rgb(249,248,249)",
+        bllack:"rgb(20,23,30)"
       }
      },
    },
